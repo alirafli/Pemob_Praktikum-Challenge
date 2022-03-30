0b9814be2082c67e0bba014dc1d2b9ccfb92f2ec
daffayanuardhana <daffayanuardhana@gmail.com>
2022-03-31 00:24:00 +0700

    daffa_[ADD] AboutUs

--- app/src/main/java/com/example/praktikum_challenge/AboutUsActivity.java
+++ app/src/main/java/com/example/praktikum_challenge/AboutUsActivity.java
@@ -2,13 +2,69 @@ package com.example.praktikum_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AboutUsActivity extends AppCompatActivity {
+
+    private ImageButton p1, p2, p3, p4, p5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        p1 = findViewById(R.id.photo_1);
+        p2 = findViewById(R.id.photo_2);
+        p3 = findViewById(R.id.photo_3);
+        p4 = findViewById(R.id.photo_4);
+        p5 = findViewById(R.id.photo_5);
+
+        p1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ig1 = new Intent(Intent.ACTION_VIEW);
+                ig1.setData(Uri.parse("https://www.instagram.com/alirafli_/"));
+                startActivity(ig1);
+            }
+        });
+
+        p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ig2 = new Intent(Intent.ACTION_VIEW);
+                ig2.setData(Uri.parse("https://www.instagram.com/alirafli_/"));
+                startActivity(ig2);
+            }
+        });
+
+        p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ig3 = new Intent(Intent.ACTION_VIEW);
+                ig3.setData(Uri.parse("https://www.instagram.com/daffayanuardhana/"));
+                startActivity(ig3);
+            }
+        });
+
+        p4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ig4 = new Intent(Intent.ACTION_VIEW);
+                ig4.setData(Uri.parse("https://www.instagram.com/alirafli_/"));
+                startActivity(ig4);
+            }
+        });
+
+        p5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ig5 = new Intent(Intent.ACTION_VIEW);
+                ig5.setData(Uri.parse("https://www.instagram.com/alirafli_/"));
+                startActivity(ig5);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/praktikum_challenge/HomepageActivity.java
+++ app/src/main/java/com/example/praktikum_challenge/HomepageActivity.java
@@ -2,7 +2,14 @@ package com.example.praktikum_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HomepageActivity extends AppCompatActivity {
 
